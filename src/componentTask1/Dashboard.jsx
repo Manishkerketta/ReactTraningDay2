ca3eb6ab24149fd4c49e79740ac9171f1d7b7b06
mastermanish <manish.k@iserveu.in>
2026-04-15 11:05:10 +0530

    added div

--- src/componentTask1/Dashboard.jsx
+++ src/componentTask1/Dashboard.jsx
@@ -1,19 +1,19 @@
 const UserProfile = ({ username, age, isAdmin}) => {
   return (
-    <>
+    <div>
       <h1>User: {username}</h1>
       <h2>Age: {age}</h2>
       {isAdmin && <h3>Admin</h3>}
-    </>
+    </div>
   );
 };
 
 export default function Dashboard() {
   return (
-    <>
+    <div>
       <UserProfile username="Manish" age={20} isAdmin={true} />
       <UserProfile username="Alex" age={25} isAdmin={false} /> 
       <UserProfile username="Sarah" age={22} isAdmin={true} />
-    </>
+    </div>
   );
 }
